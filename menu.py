472ddfc4607d2c0d06d1977aee3b0c51e6bd24e6
LuisEderLeon <luiseder.leonvesga@gmail.com>
2024-05-09 17:27:05 -0500

    Feat Desarrollo del menu

--- menu.py
+++ menu.py
@@ -9,10 +9,34 @@ servicios = json.load(fileSe)
 fileUs.close()
 fileSe.close()
 
-servicios = fSe.crearServicio(servicios)
-usuarios = fUs.crearUsuario(usuarios)
-usuarios = fUs.añadirServicio(usuarios,servicios)
-fUs.listarUsuarios(usuarios)
+print("""⠀⠀⠀⠀⠀⠀⢀⣤⣶⡾⡿⠟⠿⠿⠿⠻⣷⣶⣤⣀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣠⣾⢿⢏⡡⣄⣠⢠⣀⣄⣠⣀⣄⣉⠞⣿⢷⣤⡀⠀⠀⠀
+⠀⠀⣠⣾⢿⣽⣻⢾⣽⣞⣷⣻⣞⡾⣵⣯⢾⣭⡿⣞⣯⣿⣳⣄⠀⠀
+⠀⣴⡿⣽⣻⡾⣯⣿⢾⣽⡾⣷⣻⢿⣽⡾⣿⣽⣻⣽⣟⣾⣽⣻⣧⠀
+⢰⡿⣽⣟⡷⣿⣽⢾⣻⣷⣻⣽⢯⣿⢾⣽⡷⣯⡇⢸⡾⡽⠊⣱⢿⡇
+⣿⢿⡝⢊⣉⣉⠺⣿⠉⣷⠻⠽⠿⣽⠻⠾⢽⡷⠷⠺⣍⣠⡾⣯⣟⣿
+⣿⣻⠀⣿⢯⣟⠶⢮⠀⡧⠔⢓⠀⣿⠀⣶⡎⢰⢶⡆⠸⣏⣉⣉⣸⢿
+⣿⡽⣧⣈⣉⣉⣤⡿⣀⣇⣘⣛⣀⡿⣀⣷⣳⣈⣉⣁⣼⣻⢾⣽⢯⣿
+⠸⣟⣷⣯⢿⣽⣾⣻⢿⡽⣯⢿⣽⣻⣟⡷⣿⣽⣻⣽⢾⣻⣯⣟⣯⡇
+⠀⠻⣷⢯⣿⣞⡷⣟⣯⣿⣻⢯⡷⣟⡾⣟⣷⣯⣟⣾⢿⣽⣞⣯⡟⠀
+⠀⠀⠙⢿⣳⣯⢿⣻⣽⡾⣽⢯⣻⣝⣻⡽⣾⣳⡿⣽⣻⣾⡽⠋⠀⠀
+⠀⠀⠀⠀⠛⢽⣟⣯⡷⣟⣯⢿⣱⢯⣷⣻⣽⢷⣟⣿⡳⠛⠁⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠉⠛⠹⠿⣽⣻⣯⣿⢾⣽⠾⠟⠛⠈
+
+
+Bienvenido al menu de administrativos de Claro
+
+Ingrese una de las siguientes opciones
+--------------------------------------------------------------------
+CRUD de usuarios -> 1
+CRUD de servicios -> 2
+Añadir situacion de servicio al cliente -> 3
+Consultar ventas de un cliente -> 4
+Solicitar sugerencias de venta hacia un cliente -> 5
+Salir -> 6
+--------------------------------------------------------------------""")
+opcion = input("Opcion: ")
+print("--------------------------------------------------------------------")
 
 fileUs = open("usuarios.json","w")
 fileSe = open("servicios.json","w")
